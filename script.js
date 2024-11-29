bdec00fda9189138e87539d633c66121333eef0a
Beltran Castro <miguel.beltran.castro@gmail.com>
2024-11-29 11:22:18 +0100

    Add some variables

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// Variables
 const btn = document.querySelector(".btn-open");
 const form = document.querySelector(".fact-form");
 
+let votesInteresting = 0;
+let votesMindblowing = 0;
+let totalUpvotes = 0;
+let votesWrong = 0;
+
+// Show and Hide form
 btn.addEventListener("click", function() {
   if (form.classList.contains("hidden")) {
     form.classList.remove("hidden");
