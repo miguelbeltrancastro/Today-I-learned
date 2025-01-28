b1fdfdcd57b5ea399412630f2f4f8878d9f310b7
Miguel Beltrán Castro <miguel.beltran.castro@gmail.com>
2025-01-28 13:05:44 +0100

    list itemas as components

--- src/App.js
+++ src/App.js
@@ -85,28 +85,35 @@ function FactList(){
 
   return <section><ul className="facts-list">
     {
-      fact.map((fact)=>
-        <li key={fact.id} className="fact">
-      <p>
-        {fact.text}
-        <a
-          className="source"
-          href={fact.source}
-          target="_blank"
-          >(Source)</a
-        >
-      </p>
-      <span className="tag" style={{backgroundColor: CATEGORIES.find(cat => cat.name === fact.category).color}}
-        >{fact.category}</span
-      >
-      <div className="vote-buttons">
-        <button>👍 {fact.votesInteresting}</button>
-        <button>🤯 {fact.votesMindblowing}</button>
-        <button>⛔ {fact.votesFalse}</button>
-      </div>
-      </li>
+      fact.map((fact)=> <Fact key={fact.id} fact={fact}/>
+
     )}
-    </ul></section>;
+    </ul>
+    Ther are {fact.length} facts
+    </section>;
+}
+
+function Fact({fact}){
+  return(
+  <li className="fact">
+  <p>
+    {fact.text}
+    <a
+      className="source"
+      href={fact.source}
+      target="_blank"
+      >(Source)</a
+    >
+  </p>
+  <span className="tag" style={{backgroundColor: CATEGORIES.find(cat => cat.name === fact.category).color}}
+    >{fact.category}</span
+  >
+  <div className="vote-buttons">
+    <button>👍 {fact.votesInteresting}</button>
+    <button>🤯 {fact.votesMindblowing}</button>
+    <button>⛔ {fact.votesFalse}</button>
+  </div>
+  </li>)
 }
 
 export default App
