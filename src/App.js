213177d680ed5f1cb3bebf555de59b5bb0d43caa
Miguel Beltrán Castro <miguel.beltran.castro@gmail.com>
2025-02-01 16:17:50 +0100

    Form all done now in react

--- src/App.js
+++ src/App.js
@@ -35,15 +35,27 @@ const initialFacts = [
   },
 ];
 
+const CATEGORIES = [
+  { name: "technology", color: "#3b82f6" },
+  { name: "science", color: "#16a34a" },
+  { name: "finance", color: "#ef4444" },
+  { name: "society", color: "#eab308" },
+  { name: "entertainment", color: "#db2777" },
+  { name: "health", color: "#14b8a6" },
+  { name: "history", color: "#f97316" },
+  { name: "news", color: "#8b5cf6" },
+];
+
 function App(){
   const [showForm, setShowForm] = useState(false);
+  const [facts,setFacts] = useState(initialFacts);
   return (
     <>
       <Header showForm={showForm} setShowForm={setShowForm}/>
-      {showForm ? <NewFactForm/> : null}
+      {showForm ? <NewFactForm setFacts = {setFacts} setShowForm ={setShowForm}/> : null}
       <main className="main">
         <CategoryFilter />
-        <FactList />
+        <FactList facts={facts}/>
       </main>
     </>
   );
@@ -69,20 +81,63 @@ function Header({showForm, setShowForm}){
   );
 }
 
-function NewFactForm(){
-  return <form className="fact-form">NewFactForm</form>;
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;  
+  }
+
+  return url.protocol === "http:" || url.protocol === "https:";
 }
 
-const CATEGORIES = [
-  { name: "technology", color: "#3b82f6" },
-  { name: "science", color: "#16a34a" },
-  { name: "finance", color: "#ef4444" },
-  { name: "society", color: "#eab308" },
-  { name: "entertainment", color: "#db2777" },
-  { name: "health", color: "#14b8a6" },
-  { name: "history", color: "#f97316" },
-  { name: "news", color: "#8b5cf6" },
-];
+function NewFactForm({setFacts, setShowForm}){
+  const [text, setText] = useState("");
+  const textLentght = text.length;
+  const [source, setSource] = useState("http://example.com");
+  const [category, setCategory] = useState("  ");
+
+  function handleSubmit(e){
+    // 1. Preven browser reload
+    e.preventDefault();
+    // 2. check if data is valid, create a new fact
+    if (text && isValidHttpUrl(source) && category && textLentght <= 200) {
+      //3. create a new fact object
+      const newFact = {
+        id: Math.round(Math.random()*1000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),}
+      //4. add new fact to the UI
+        setFacts((facts)=>[newFact, ...facts])
+      //5. reset input fields
+      setText("");
+      setSource("");
+      setCategory("");
+      //6. close form
+      setShowForm(false);
+    }
+  }
+  return(
+    <form className="fact-form" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Share a fact with the world..." value = {text} onChange={(e) =>setText(e.target.value)}/>
+        <span>
+          {200 - textLentght}
+        </span>
+        <input type="text" placeholder="Trustworthy source" value = {source} onChange={(e) =>setSource(e.target.value)}/>
+        <select value = {category} onChange={(e) =>setCategory(e.target.value)}>
+          <option value="">Choose category</option>
+          {CATEGORIES.map((cat) => <option key = {cat.name} value={cat.name}>{cat.name.toUpperCase()}</option>)}
+        </select>
+        <button className="btn btn-large">Post</button>
+    </form>
+  );
+}
 
 function CategoryFilter(){
   return (
@@ -101,16 +156,13 @@ function CategoryFilter(){
   );
 }
 
-function FactList(){
-  // TEMPORARY for fake data
-  const fact = initialFacts;
-
+function FactList({facts}){
   return (
     <section>
       <ul className="facts-list">
-        {fact.map((fact)=> <Fact key={fact.id} fact={fact}/>)}
+        {facts.map((fact)=> <Fact key={fact.id} fact={fact}/>)}
       </ul>
-      Ther are {fact.length} facts in the database. Add your own!
+      Ther are {facts.length} facts in the database. Add your own!
     </section>
   );
 }
